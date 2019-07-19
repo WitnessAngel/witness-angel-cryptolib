8e770287258b49ab709f52f0161bbe2df0202248
ManonMic <manon.michelet5@gmail.com>
2019-07-19 15:11:57 +0200

    Organize the module.

--- tests/test_wacryptolib.py
+++ tests/test_wacryptolib.py
@@ -1,108 +1,36 @@
-import uuid
 from datetime import datetime
 
-from Crypto.Cipher import PKCS1_OAEP
-from Crypto.PublicKey import DSA, ECC, RSA
+from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 
 import wacryptolib
 
 
-def test_generate_rsa_keypair():
-    """Cipher then decipher a message using RSA keypair"""
-
-    uid = None
-    binary_content = "Mon hât èst joli".encode("utf-8")
-
-    keys = wacryptolib.generate_rsa_keypair(uid)
-    public_key = keys["public_key"]
-    private_key = keys["private_key"]
-
-    # Cipher the binary content with the public key
-    cipher = PKCS1_OAEP.new(public_key)
-    ciphertext = cipher.encrypt(binary_content)
-
-    # Decipher it with the private key
-    decipher = PKCS1_OAEP.new(private_key)
-    deciphertext = decipher.decrypt(ciphertext)
-
-    try:  # FIXME
-        assert deciphertext == binary_content
-        print("Generating keypair : successfuly done")
-    except AssertionError:
-        print("Problem cccured in the deciphering")
-
-
-def test_generate_dsa_keypair():
-    """Check if the function generate_dsa_keypair returns
-    a pair of DSA keys"""
-
-    uid = None
-    keypair = wacryptolib.generate_dsa_keypair(uid)
-    public_key = keypair["public_key"]
-    private_key = keypair["private_key"]
-    assert isinstance(public_key, DSA.DsaKey), type(public_key)
-    assert isinstance(private_key, DSA.DsaKey), type(private_key)
-
-
-def test_generate_ecc_keypair():
-    """Check if the function generate_ecc_keypair returns
-    a pair of ECC keys"""
-
-    uid = None
-    keypair = wacryptolib.generate_ecc_keypair(uid, curve="p256")
-    public_key = keypair["public_key"]
-    private_key = keypair["private_key"]
-    assert isinstance(public_key, ECC.EccKey), type(public_key)
-    assert isinstance(private_key, ECC.EccKey), type(private_key)
-
-
 def test_generate_shared_secret():
-    """Cipher then decipher a message using shared secret and RSA"""
+    keypair = wacryptolib.key_generation.generate_rsa_keypair(None)
+    public_key, private_key = wacryptolib.key_generation.rsakey_to_bytes(keypair)
 
-    uid = uuid.uuid4()
-    keys_count = 3
-    threshold_count = 2
-
-    binary_content = "Mon hât èst joli".encode("utf-8")
-    keys_info = wacryptolib.generate_shared_secret_key(
-        uid, keys_count, threshold_count=threshold_count
+    keys_info = wacryptolib.sharedsecret.generate_shared_secret_key(
+        public_key=public_key,
+        private_key=private_key,
+        shares_count=3,
+        threshold_count=2,
     )
-    shares = keys_info["shares"]
-    public_key = keys_info["public_key"]
-    shares = wacryptolib.split_as_padded_chunks(shares, 3)
 
-    # Cipher the binary content
-    cipher = PKCS1_OAEP.new(public_key)
-    ciphertext = cipher.encrypt(binary_content)
+    private_key_reconstructed = wacryptolib.sharedsecret.reconstruct_shared_secret_key(
+        keys_info["shares"], shares_count=3
+    )
 
-    # Combine all the shares to make a list of bytes corresponding to the private key
-    combined_shares_list = wacryptolib.recombine_shares_into_bytestring(shares)
-
-    # Unpad the last tuple
-    combined_shares_list = wacryptolib.unpad_last_element(combined_shares_list)
-
-    # Reconstruct the private key in type bytes
-    private_key_reconstructed = b"".join(combined_shares_list)
-
-    # decipher the binary content
-    decipher = PKCS1_OAEP.new(RSA.import_key(private_key_reconstructed))
-    deciphertext = decipher.decrypt(ciphertext)
-
-    try:
-        assert binary_content == deciphertext
-        print("Shared secret : successfuly done")
-    except AssertionError:
-        print("Problem cccured in the deciphering")
+    assert private_key_reconstructed == private_key
 
 
 def test_sign_and_verify_rsa():
-    keypair = wacryptolib.generate_rsa_keypair(None)
-    data_hash, signature = wacryptolib.sign_with_rsa(
-        private_key=RSA.RsaKey.export_key(keypair["private_key"]), data=b"Hello"
+    keypair = wacryptolib.key_generation.generate_rsa_keypair(None)
+    data_hash, signature = wacryptolib.signature.sign_rsa(
+        private_key=RSA.RsaKey.export_key(keypair["private_key"]), plaintext=b"Hello"
     )
 
-    wacryptolib.verify_authenticity_rsa_signature(
+    wacryptolib.signature.verify_rsa_signature(
         public_key=RSA.RsaKey.export_key(keypair["public_key"]),
         data_hash=data_hash,
         signature=signature,
@@ -114,38 +42,46 @@ def test_aes_cbc():
 
     binary_content = "Mon hât èst joli".encode("utf-8")
 
-    cipher_text = wacryptolib.encrypt_via_aes_cbc(key=key, data=binary_content)
+    cipher_text = wacryptolib.cipher.encrypt_via_aes_cbc(
+        key=key, plaintext=binary_content
+    )
 
-    decipher_text = wacryptolib.decrypt_via_aes_cbc(key=key, data=cipher_text)
+    decipher_text = wacryptolib.cipher.decrypt_via_aes_cbc(
+        key=key, ciphertext=cipher_text
+    )
 
-    try:
-        assert decipher_text == binary_content
-        print("Ciphering and deciphering with CBC mode : successfuly done")
-    except AssertionError:
-        print("Problem cccured in the deciphering")
+    assert decipher_text == binary_content
+
+
+def test_aes_eax():
+    key = get_random_bytes(16)
+
+    binary_content = "Mon hât èst joli".encode("utf-8")
+
+    ciphertext, tag, nonce = wacryptolib.cipher.encrypt_via_aes_eax(
+        key=key, plaintext=binary_content
+    )
+
+    wacryptolib.cipher.decrypt_via_aes_eax(
+        key=key, ciphertext=ciphertext, tag=tag, nonce=nonce
+    )
 
 
 def test_sign_dsa():
-    keypair = wacryptolib.generate_dsa_keypair(None)
+    keypair = wacryptolib.key_generation.generate_dsa_keypair(None)
     public_key = keypair["public_key"]
     private_key = keypair["private_key"]
     binary_content = "Mon hât èst joli".encode("utf-8")
-    timestamp_verifier = int(datetime.timestamp(datetime.now()))
+    timestamp_verifier = datetime.timestamp(datetime.now())
 
-    signature, timestamp = wacryptolib.sign_data_dsa(
-        private_key=private_key, data=binary_content
+    signature, timestamp = wacryptolib.signature.sign_dsa(
+        private_key=private_key, plaintext=binary_content
     )
-    verifier, hash_obj = wacryptolib.generate_verifier(
-        public_key=public_key, data=binary_content
+    wacryptolib.signature.verify_dsa_signature(
+        public_key=public_key,
+        plaintext=binary_content,
+        signature=signature,
+        timestamp=timestamp,
     )
-    wacryptolib.verify_authenticity(hash_obj, signature, verifier)
-    assert timestamp == timestamp_verifier, "modification détectée"
 
-
-if __name__ == "__main__":
-    # test_generate_shared_secret()
-    # test_generate_rsa_keypair()
-    # test_generate_dsa_keypair()
-    # test_generate_ecc_keypair()
-    # test_aes_cbc()
-    test_sign_dsa()
+    assert timestamp == timestamp_verifier, "timestamps don't correspond"

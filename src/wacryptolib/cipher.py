f59022d5c41e5d51ab507480f4a47a2f5d4d065d
Pascal Chambon <pythoniks@gmail.com>
2019-08-29 16:37:24 +0200

    Refactor cipher utilities.

--- src/wacryptolib/cipher.py
+++ src/wacryptolib/cipher.py
@@ -4,133 +4,129 @@ from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, ChaCha20_Poly1305, PKCS1_OAEP
 from Crypto.Util.Padding import pad, unpad
 from Crypto.PublicKey import RSA
+import Crypto.Hash
 
 
-# ---- AES/CBC ----
 def encrypt_via_aes_cbc(key: bytes, plaintext: bytes) -> dict:
-    """Permits to encrypt a `plaintext` thanks to a `key` with the CBC mode
+    """Encrypt a bytestring using AES (CBC mode).
 
-    :param key: the cryptographic key which will serve to decipher the cipher text
-    :param plaintext: the text to cipher
+    :param key: AES cryptographic key of a proper length for chose AES flavour.
+    :param plaintext: the bytes to cipher
 
-    :return: concatenation of initialization vector and ciphertext encoded in base 64"""
+    :return: dict with fields "iv" and "ciphertext" as base64 strings"""
 
     iv = get_random_bytes(AES.block_size)
     cipher = AES.new(key, AES.MODE_CBC, iv)
-    ciphertext = cipher.encrypt(pad(plaintext, AES.block_size))
+    ciphertext = cipher.encrypt(pad(plaintext, block_size=AES.block_size))
     iv_and_ciphertext = {"iv": b64encode(iv), "ciphertext": b64encode(ciphertext)}
     return iv_and_ciphertext
 
 
 def decrypt_via_aes_cbc(key: bytes, iv_and_ciphertext: dict) -> bytes:
-    """Permits to decrypt a `ciphertext` in base 64 thanks to a `key`
+    """Decrypt a bytestring using AES (CBC mode).
 
-    :param key: the cryptographic key which will serve to cipher the plain text
-    :param iv_and_ciphertext: concatenation of the initialization vector and the text to decipher
+    :param key: the cryptographic key used to decipher
+    :param iv_and_ciphertext: dict with fields "iv" and "ciphertext" as base64 strings
 
-    :return: The decrypted text as bytes"""
+    :return: the decrypted bytestring"""
 
     iv = iv_and_ciphertext["iv"]
     ciphertext = iv_and_ciphertext["ciphertext"]
     decipher = AES.new(key, AES.MODE_CBC, b64decode(iv))
-    decrypted_text = unpad(decipher.decrypt(b64decode(ciphertext)), AES.block_size)
-    return decrypted_text
+    plaintext = unpad(decipher.decrypt(b64decode(ciphertext)), block_size=AES.block_size)
+    return plaintext
 
 
-# ---- AES/EAX ----
 def encrypt_via_aes_eax(key: bytes, plaintext: bytes) -> dict:
-    """Permits to encrypt a `plaintext` thanks to a `key` with the EAX mode
+    """Encrypt a bytestring using AES (EAX mode).
 
-    :param key: the cryptographic key which will serve to cipher the plain text
-    :param plaintext: the text to cipher
+    :param key: AES cryptographic key of a proper length for chose AES flavour.
+    :param plaintext: the bytes to cipher
 
-    :return: the ciphertext as bytes, the tag and the nonce"""
+    :return: dict with fields "ciphertext", "tag" and "nonce" as base64 strings"""
 
     cipher = AES.new(key, AES.MODE_EAX)
     nonce = cipher.nonce
     ciphertext, tag = cipher.encrypt_and_digest(plaintext)
-    encryption = {"ciphertext": ciphertext, "tag": tag, "nonce": nonce}
+    encryption = {"ciphertext": b64encode(ciphertext), "tag": b64encode(tag), "nonce": b64encode(nonce)}
     return encryption
 
 
 def decrypt_via_aes_eax(key: bytes, encryption: dict) -> bytes:
-    """Permits to decrypt a `ciphertext` in base 64 thanks to a `key`
+    """Decrypt a bytestring using AES (EAX mode).
 
-    :param key: the cryptographic key which will serve to decipher the cipher text
-    :param encryption: dictionary composed of a ciphertext, tag and nonce
+    :param key: the cryptographic key used to decipher
+    :param encryption: dict with fields "ciphertext", "tag" and "nonce" as base64 strings
 
-    :return: the text deciphered"""
+    :return: the decrypted bytestring"""
 
-    decipher = AES.new(key, AES.MODE_EAX, nonce=encryption["nonce"])
-    deciphertext = decipher.decrypt(encryption["ciphertext"])
-    decipher.verify(encryption["tag"])
-    return deciphertext
+    decipher = AES.new(key, AES.MODE_EAX, nonce=b64decode(encryption["nonce"]))
+    plaintext = decipher.decrypt(b64decode(encryption["ciphertext"]))
+    decipher.verify(b64decode(encryption["tag"]))
+    return plaintext
 
 
-# ---- ChaCha20 ----
 def encrypt_via_chacha20_poly1305(
     key: bytes, plaintext: bytes, header: bytes = b"header"
 ) -> dict:
-    """Permits to encrypt a `plaintext` thanks to a `key` of 32 bytes long
-    with ChaCha20 which is a stream cipher.
+    """Encrypt a bytestring with the stream cipher ChaCha20.
 
-    :param header: additional authenticated data (AAD)
-    :param key: the cryptographic key which will serve to cipher the plain text
-    :param plaintext: the text to cipher
+    :param key: 32 bytes long cryptographic key
+    :param plaintext: the bytes to cipher
+    :param header: optional "additional authenticated data" (AAD)
 
-    :return: the ciphertext as bytes and the nonce"""
+    :return: dict with fields "ciphertext", "tag", "nonce" and "header" as base64 strings"""
 
     cipher = ChaCha20_Poly1305.new(key=key)
     cipher.update(header)
     ciphertext, tag = cipher.encrypt_and_digest(plaintext)
     nonce = cipher.nonce
     encryption = {
-        "ciphertext": ciphertext,
-        "tag": tag,
-        "nonce": nonce,
-        "header": header,
+        "ciphertext": b64encode(ciphertext),
+        "tag": b64encode(tag),
+        "nonce": b64encode(nonce),
+        "header": b64encode(header),
     }
     return encryption
 
 
 def decrypt_via_chacha20_poly1305(key: bytes, encryption: dict) -> bytes:
-    """Permits to decrypt a `ciphertext` thanks to a `key` of 32 bytes long
+    """Decrypt a bytestring with the stream cipher ChaCha20.
 
-    :param encryption: dictionary with a ciphertext, tag, nonce and header
-    :param key: cryptographic key which will serve to decipher the cipher text
+    :param key: the cryptographic key used to decipher
+    :param encryption: dict with fields "ciphertext", "tag", "nonce" and "header" as base64 strings
 
-    :return: the text deciphered"""
+    :return: the decrypted bytestring"""
 
-    decipher = ChaCha20_Poly1305.new(key=key, nonce=encryption["nonce"])
-    decipher.update(encryption["header"])
-    deciphertext = decipher.decrypt_and_verify(
-        ciphertext=encryption["ciphertext"], received_mac_tag=encryption["tag"]
+    decipher = ChaCha20_Poly1305.new(key=key, nonce=b64decode(encryption["nonce"]))
+    decipher.update(b64decode(encryption["header"]))
+    plaintext = decipher.decrypt_and_verify(
+        ciphertext=b64decode(encryption["ciphertext"]), received_mac_tag=b64decode(encryption["tag"])
     )
-    return deciphertext
+    return plaintext
 
 
-# ---- PKCS#1 OAEP ----
 def encrypt_via_rsa_oaep(key: RSA.RsaKey, plaintext: bytes) -> bytes:
-    """Permits to encrypt a `plaintext` thanks to a public RSA key
+    """Encrypt a bytestring with PKCS#1 RSA OAEP (asymmetric algo).
 
-    :param key: the cryptographic key which will serve to cipher the plain text
-    :param plaintext: the text to cipher
+    :param key: public RSA key
+    :param plaintext: the bytes to cipher
 
     :return: the ciphertext as bytes"""
 
-    cipher = PKCS1_OAEP.new(key=key)
+    cipher = PKCS1_OAEP.new(key=key, hashAlgo=Crypto.Hash.SHA256)
     ciphertext = cipher.encrypt(plaintext)
-    return ciphertext
+    return b64encode(ciphertext)
 
 
-def decrypt_via_rsa_oaep(key: RSA.RsaKey, encryption: bytes) -> bytes:
-    """Permits to decrypt a `ciphertext` thanks to a private RSA key
+def decrypt_via_rsa_oaep(key: RSA.RsaKey, ciphertext: bytes) -> bytes:
+    """Decrypt a bytestring with PKCS#1 RSA OAEP (asymmetric algo).
 
-    :param key: the cryptographic key which will serve to decipher the cipher text
+    :param key: private RSA key
     :param ciphertext: the text to decipher
 
-    :return: data deciphered"""
+    :return: the decrypted bytestring"""
 
-    decipher = PKCS1_OAEP.new(key)
-    deciphertext = decipher.decrypt(ciphertext=encryption)
-    return deciphertext
+    decipher = PKCS1_OAEP.new(key, hashAlgo=Crypto.Hash.SHA256)
+    plaintext = decipher.decrypt(ciphertext=b64decode(ciphertext))
+    return plaintext
---
+++ tests/test_wacryptolib_cipher.py
@@ -0,0 +1,76 @@
+import random
+
+from Crypto.PublicKey import RSA, ECC, DSA
+from Crypto.Random import get_random_bytes
+
+import uuid
+
+import wacryptolib
+
+
+def _get_binary_content():
+    bytes_length = random.randint(1, 100)
+    return get_random_bytes(bytes_length)
+
+
+def test_aes_cbc_encryption_and_decryption():
+    key = get_random_bytes(16)
+
+    binary_content = _get_binary_content()
+
+    iv_and_ciphertext = wacryptolib.cipher.encrypt_via_aes_cbc(
+        key=key, plaintext=binary_content
+    )
+
+    decrypted_content = wacryptolib.cipher.decrypt_via_aes_cbc(
+        key=key, iv_and_ciphertext=iv_and_ciphertext
+    )
+
+    assert decrypted_content == binary_content
+
+
+def test_aes_eax_encryption_and_decryption():
+    key = get_random_bytes(16)
+
+    binary_content = _get_binary_content()
+
+    encryption = wacryptolib.cipher.encrypt_via_aes_eax(
+        key=key, plaintext=binary_content
+    )
+
+    decrypted_content = wacryptolib.cipher.decrypt_via_aes_eax(key=key, encryption=encryption)
+
+    assert decrypted_content == binary_content
+
+
+def test_chacha20_symetric_encryption_and_decryption():
+    key = get_random_bytes(32)  # ONLY length allowed for chacha20
+
+    binary_content = _get_binary_content()
+
+    encryption = wacryptolib.cipher.encrypt_via_chacha20_poly1305(
+        key, binary_content, header=b"header"
+    )
+
+    decrypted_content = wacryptolib.cipher.decrypt_via_chacha20_poly1305(
+        key=key, encryption=encryption
+    )
+
+    assert decrypted_content == binary_content
+
+
+def test_rsa_oaep_encryption_and_decryption():
+    uid = uuid.uuid4()
+    keypair = wacryptolib.key_generation._generate_rsa_keypair_as_objects(uid, key_length=2048)
+
+    binary_content = _get_binary_content()
+
+    ciphertext = wacryptolib.cipher.encrypt_via_rsa_oaep(
+        key=keypair["public_key"], plaintext=binary_content
+    )
+
+    decrypted_content = wacryptolib.cipher.decrypt_via_rsa_oaep(
+        key=keypair["private_key"], ciphertext=ciphertext
+    )
+
+    assert decrypted_content == binary_content

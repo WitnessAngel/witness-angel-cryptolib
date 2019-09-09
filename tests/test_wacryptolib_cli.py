46ded20085297e377ee2147aee26d84729e5f52f
Pascal Chambon <pythoniks@gmail.com>
2019-09-09 22:38:46 +0200

    Test the wacryptolib command line interface.

---
+++ tests/test_wacryptolib_cli.py
@@ -0,0 +1,64 @@
+import os
+
+from click.testing import CliRunner
+from wacryptolib.__main__ import cli
+
+
+def test_cli_help_texts():
+    runner = CliRunner()
+
+    result = runner.invoke(cli, ["-h"])
+    assert result.exit_code == 0
+    assert "Usage:" in result.output
+
+    result = runner.invoke(cli, ["encrypt", "-h"])
+    assert result.exit_code == 0
+    assert "into a secure container" in result.output
+
+    result = runner.invoke(cli, ["decrypt", "-h"])
+    assert result.exit_code == 0
+    assert "original media file" in result.output
+
+
+def test_cli_encryption_and_decryption():
+    runner = CliRunner()
+
+    data_file = "test_file.txt"
+    data_sample = "Héllô\nguÿs"
+
+    with runner.isolated_filesystem() as tempdir:
+
+        print ("TEMPORARY TEST DIRECTORY:", tempdir)
+
+        with open(data_file, "w") as f:
+            f.write(data_sample)
+
+        result = runner.invoke(cli, ["encrypt", "-i", "test_file.txt"])
+        assert result.exit_code == 0
+        assert os.path.exists(data_file + ".crypt")
+
+        result = runner.invoke(cli, ["encrypt", "-i", "test_file.txt", "-o", "stuff.dat"])
+        assert result.exit_code == 0
+        assert os.path.exists("stuff.dat")
+
+        os.remove(data_file)  # CLEANUP
+        assert not os.path.exists(data_file)
+
+        result = runner.invoke(cli, ["decrypt", "-i", data_file + ".crypt"])
+        assert result.exit_code == 0
+        assert os.path.exists(data_file)
+
+        result = runner.invoke(cli, ["decrypt", "-i", "stuff.dat"])
+        assert result.exit_code == 0
+        assert os.path.exists("stuff.dat.medium")
+
+        result = runner.invoke(cli, ["decrypt", "-i", "stuff.dat", "-o", "stuffs.txt"])
+        assert result.exit_code == 0
+        assert os.path.exists("stuffs.txt")
+
+        for result_file in (data_file, "stuff.dat.medium", "stuffs.txt"):
+            with open(result_file, "r") as f:
+                data = f.read()
+                assert data == data_sample
+
+

0018ea8f652cdd7f9ce150634970572c8f6ceedb
Pascal Chambon <pythoniks@gmail.com>
2019-09-15 20:04:45 +0200

    Fix packaging to rely only on pyproject.toml and poetry.

--- docs/conf.py
+++ docs/conf.py
@@ -13,19 +13,21 @@
 import os
 import sys
 import pathlib
+import toml
 
+root_dir = pathlib.Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(root_dir / "src"))
 
-sys.path.insert(0, str(pathlib.Path(__file__).resolve().parents[1] / "src"))
-
+_metadata = (toml.load(str(root_dir / "pyproject.toml")))
 
 # -- Project information -----------------------------------------------------
 
-project = "Witness Angel"
-copyright = "2019, Manon"
-author = "Manon"
+project = _metadata["tool"]["poetry"]["name"]
+author = ", ".join(_metadata["tool"]["poetry"]["authors"])
+copyright = "Witness Angel Project"
 
 # The short X.Y version.
-version = open("../VERSION").read()
+version = _metadata["tool"]["poetry"]["version"]
 # The full version, including alpha/beta/rc tags.
 release = version
 
@@ -34,7 +36,7 @@ release = version
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx_autodoc_typehints"]
+extensions = ["sphinx.ext.autodoc", "sphinx_autodoc_typehints", "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -50,7 +52,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

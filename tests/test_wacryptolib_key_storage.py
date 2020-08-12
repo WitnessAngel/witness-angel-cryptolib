bdb78262057519a2e186bfad486c55ec7ac241a7
Akram Bourichi <bourichiakram@gmail.com>
2020-08-12 18:28:47 +0200

    Add test_list_keys

--- tests/test_wacryptolib_key_storage.py
+++ tests/test_wacryptolib_key_storage.py
@@ -60,3 +60,47 @@ def test_key_storages_free_keys_concurrency(tmp_path):
 
     for key_storage in (dummy_key_storage, filesystem_key_storage):
         check_key_storage_free_keys_concurrency(key_storage)
+
+
+@pytest.mark.parametrize(
+    "key_type", ['DSA_DSS', 'ECC_DSS', 'RSA_OAEP', 'RSA_PSS']
+)
+def test_list_keys(tmp_path,key_type):
+    
+  
+    from wacryptolib.key_generation import generate_asymmetric_keypair
+    from wacryptolib.utilities import generate_uuid0
+    key_pair = generate_asymmetric_keypair(
+                    key_type=key_type,
+                    passphrase="abc".encode(),
+                )  
+            
+    key_pairs_dir=str(tmp_path)            
+    object_FilesystemKeyStorage = FilesystemKeyStorage(key_pairs_dir)
+
+    object_FilesystemKeyStorage.set_keys(
+        keychain_uid=generate_uuid0(),
+        key_type=key_type,
+        public_key=key_pair["public_key"],
+        private_key=key_pair["private_key"],
+    )
+
+    public_key_list=object_FilesystemKeyStorage.list_keys()
+    
+    assert isinstance(public_key_list, list)
+
+    assert public_key_list, "No public key detected during test"
+
+    for public_key in public_key_list:
+        print(">> public key detected:", public_key)
+        assert isinstance(public_key, dict) or isinstance(public_key, None)
+
+        assert isinstance(public_key["keychain_uid"], str)
+        
+        assert (public_key["private_key_present"] == True) or (
+            public_key["private_key_present"] == False
+        )
+        
+        assert public_key["key_type"] in ('DSA_DSS', 'ECC_DSS', 'RSA_OAEP', 'RSA_PSS')
+
+       

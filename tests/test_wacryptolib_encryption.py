3754dd12939ce9191d8be0f0c51e240d328d5e1c
Mossroy <mossroy@mossroy.fr>
2021-10-16 16:30:35 +0200

    Implement the unit test for authenticated algos

--- tests/test_wacryptolib_encryption.py
+++ tests/test_wacryptolib_encryption.py
@@ -180,23 +180,23 @@ def test_stream_manager():
 @pytest.mark.parametrize("encryption_algo", AUTHENTICATED_ENCRYPTION_ALGOS)
 def test_symmetric_decryption_verify_for_authenticated_algo(encryption_algo):
 
-    print(encryption_algo)
+    key_dict = generate_symmetric_key_dict(encryption_algo)
+    binary_content = _get_binary_content()
 
-#    key_dict = generate_symmetric_key_dict(encryption_algo)
-#    binary_content = _get_binary_content()
-#
-#    cipherdict = wacryptolib.encryption.encrypt_bytestring(
-#        key_dict=key_dict, plaintext=binary_content, encryption_algo=encryption_algo
-#    )
-#
-#    assert "ciphertext" in cipherdict  # Mandatory field
-#    assert isinstance(cipherdict["ciphertext"], bytes)
-#
-#    if encryption_algo == "AES_EAX":
-#        cipherdict["tag"] =
-#
-#    decrypted_content = wacryptolib.encryption.decrypt_bytestring(
-#        key_dict=key_dict, cipherdict=cipherdict, encryption_algo=encryption_algo, verify=False
-#    )
-#
-#    assert decrypted_content == binary_content
+    cipherdict = wacryptolib.encryption.encrypt_bytestring(
+        key_dict=key_dict, plaintext=binary_content, encryption_algo=encryption_algo
+    )
+
+    # We change the order of bytes of the tag, to make it wrong
+    cipherdict["tag"] = cipherdict["tag"][1:] + bytes(cipherdict["tag"][0])
+
+    # Decryption should not fail if verify==False
+    decrypted_content = wacryptolib.encryption.decrypt_bytestring(
+        key_dict=key_dict, cipherdict=cipherdict, encryption_algo=encryption_algo, verify=False
+    )
+
+    # Decryption should fail if verify==True
+    with pytest.raises(DecryptionError):
+        wacryptolib.encryption.decrypt_bytestring(
+            key_dict=key_dict, cipherdict=cipherdict, encryption_algo=encryption_algo, verify=True
+        )

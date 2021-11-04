f743fb11fbab400703c4a29784cc12a7f31afcdf
Pascal Chambon <pythoniks@gmail.com>
2021-11-04 17:53:36 +0100

    Update build files for python3.7 and metadata

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,7 +17,7 @@ def _generate_asymmetric_keypair_caching():
     import wacryptolib.key_generation
     original_generator = wacryptolib.key_generation._do_generate_asymmetric_keypair
     wacryptolib.key_generation.__original_do_generate_asymmetric_keypair = original_generator
-    cached_generator = functools.lru_cache(original_generator)
+    cached_generator = functools.lru_cache(maxsize=None)(original_generator)
 
     patcher = patch('wacryptolib.key_generation._do_generate_asymmetric_keypair', cached_generator)
     wacryptolib.key_generation.__original_do_generate_asymmetric_keypair_patcher = patcher  # To use stop()/start() in tests

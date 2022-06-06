ed1506c73bea10cf266a243a7d194f351f0c0fff
Pascal Chambon <pythoniks@gmail.com>
2022-06-06 23:29:06 +0200

    Patch strxor() and main hashers in pycryptodome if C extensions are missing

--- src/wacryptolib/backends/__init__.py
+++ src/wacryptolib/backends/__init__.py
@@ -28,11 +28,45 @@ if use_fallback_backend :
             raise ImportError("Fake ImportError to force failure on non-vital extension %s" % name)
         return FakePycryptodomeExtensionLib()
 
-
     assert not sys.modules.get("Crypto.Util._raw_api")  # Must NOT have been already imported by another submodule
     import Crypto.Util._raw_api
+    assert Crypto.Util._raw_api.load_pycryptodome_raw_lib
     Crypto.Util._raw_api.load_pycryptodome_raw_lib = fake_load_pycryptodome_raw_lib
 
+
+    def patched_strxor(term1, term2, output=None):
+        return bytes([_a ^ _b for (_a, _b) in zip(term1, term2)])
+    assert not sys.modules.get("Crypto.Util.strxor")
+    import Crypto.Util.strxor
+    assert Crypto.Util.strxor.strxor
+    Crypto.Util.strxor.strxor = patched_strxor
+
+
+    import hashlib, importlib
+    def _generate_patched_hasher(hash_algo):
+        hasher_factory = getattr(hashlib, hash_algo.lower())
+        class PatchedHasherClass:
+            def __init__(self, *args, **kwargs):
+                self._hasher = hasher_factory(*args, **kwargs)
+            def update(self, msg):
+                return self._hasher.update(msg)
+            def digest(self):
+                return self._hasher.digest()
+            def copy(self):
+                return self._hasher.copy()
+            def new(self, *args, **kwargs):
+                return hasher_factory(*args, **kwargs)
+        return PatchedHasherClass
+
+    for patchable_hash_algo in ["SHA1", "MD5", "SHA512"]:
+        patched_hash_class = _generate_patched_hasher(patchable_hash_algo)
+        module = importlib.import_module("Crypto.Hash.%s" % patchable_hash_algo)
+        class_name = patchable_hash_algo + "Hash"
+        assert hasattr(module, class_name), (module, class_name)
+        setattr(module, class_name, patched_hash_class)
+        setattr(module, "new", lambda *args, patched_hash_class=patched_hash_class, **kwargs: patched_hash_class(*args, **kwargs))
+        if hasattr(module, "_pbkdf2_hmac_assist"):
+            del module._pbkdf2_hmac_assist  # Force slow code path, not requiring advanced hasher capabilities
 
 from .pycryptodome import encrypt_via_aes_cbc, decrypt_via_aes_cbc, encrypt_via_aes_eax, decrypt_via_aes_eax,\
     encrypt_via_chacha20_poly1305, decrypt_via_chacha20_poly1305, build_rsa_oaep_cipher,\

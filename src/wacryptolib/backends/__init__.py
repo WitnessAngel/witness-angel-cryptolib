2275df8b216447270cfd233f2d40264d15c85479
Pascal Chambon <pythoniks@gmail.com>
2022-06-08 11:00:48 +0200

    Remove useless fallback.py file and complete the patching of Hashers in pycryptodome

--- src/wacryptolib/backends/__init__.py
+++ src/wacryptolib/backends/__init__.py
@@ -10,15 +10,14 @@ except ImportError:
 
 if use_fallback_backend :
 
-    print("Couldn't import full pycryptodome utilities, falling back to fake C extensions")
+    print("Couldn't import full pycryptodome utilities, injecting fake C extensions")
 
-    # MONKEY PATCH PYCRYPTODOME INTERNALS TO BYPASS ".so" objects
+    # WE MONKEY PATCH PYCRYPTODOME INTERNALS TO BYPASS SOME ".so" objects #
+
     class FakePycryptodomeExtensionLib:
-
         @staticmethod
         def have_aes_ni():
             return False
-
         @staticmethod
         def have_clmul():
             return False
@@ -33,14 +32,12 @@ if use_fallback_backend :
     assert Crypto.Util._raw_api.load_pycryptodome_raw_lib
     Crypto.Util._raw_api.load_pycryptodome_raw_lib = fake_load_pycryptodome_raw_lib
 
-
     def patched_strxor(term1, term2, output=None):
         return bytes([_a ^ _b for (_a, _b) in zip(term1, term2)])
     assert not sys.modules.get("Crypto.Util.strxor")
     import Crypto.Util.strxor
     assert Crypto.Util.strxor.strxor
     Crypto.Util.strxor.strxor = patched_strxor
-
 
     import hashlib, importlib
     def _generate_patched_hasher(hash_algo):
@@ -58,10 +55,12 @@ if use_fallback_backend :
                 return hasher_factory(*args, **kwargs)
         return PatchedHasherClass
 
-    for patchable_hash_algo in ["SHA1", "MD5", "SHA512"]:
+    PATCHABLE_HASH_ALGOS = ["SHA1", "MD5", "SHA512", "SHA256", "SHA512", "SHA3_256", "SHA3_512"]  # Must be bigger than SUPPORTED_HASH_ALGOS of wacryptolib
+
+    for patchable_hash_algo in PATCHABLE_HASH_ALGOS:
         patched_hash_class = _generate_patched_hasher(patchable_hash_algo)
         module = importlib.import_module("Crypto.Hash.%s" % patchable_hash_algo)
-        class_name = patchable_hash_algo + "Hash"
+        class_name = patchable_hash_algo + ("_" if "_" in patchable_hash_algo else "") + "Hash"
         assert hasattr(module, class_name), (module, class_name)
         setattr(module, class_name, patched_hash_class)
         setattr(module, "new", lambda *args, patched_hash_class=patched_hash_class, **kwargs: patched_hash_class(*args, **kwargs))
@@ -99,40 +98,8 @@ if use_fallback_backend :
     Crypto.Cipher.AES.new = patched_aes_new
 
 
-from .pycryptodome import encrypt_via_aes_cbc, decrypt_via_aes_cbc, encrypt_via_aes_eax, decrypt_via_aes_eax,\
-    encrypt_via_chacha20_poly1305, decrypt_via_chacha20_poly1305, build_rsa_oaep_cipher,\
-    build_aes_cbc_cipher, build_aes_eax_cipher, build_chacha20_poly1305_cipher, AES_BLOCK_SIZE
+from .pycryptodome import encrypt_via_aes_cbc, decrypt_via_aes_cbc, encrypt_via_aes_eax, decrypt_via_aes_eax, encrypt_via_chacha20_poly1305, decrypt_via_chacha20_poly1305, build_rsa_oaep_cipher, build_aes_cbc_cipher, build_aes_eax_cipher, build_chacha20_poly1305_cipher, AES_BLOCK_SIZE
 from .pycryptodome import generate_rsa_keypair, generate_dsa_keypair, generate_ecc_keypair, import_rsa_key_from_pem, import_dsa_key_from_pem, import_ecc_key_from_pem, export_rsa_key_to_pem, export_dsa_key_to_pem, export_ecc_key_to_pem, rsa_key_class_fetcher, dsa_key_class_fetcher, ecc_key_class_fetcher
 from .pycryptodome import get_random_bytes, pad, unpad, get_hasher_instance
 from .pycryptodome import shamir_split, shamir_combine
 from .pycryptodome import sign_with_pss, verify_with_pss, sign_with_dss, verify_with_dss
-
-
-if use_fallback_backend:
-
-    from  .fallback import get_hasher_instance
-    ##from .fallback import AES_BLOCK_SIZE
-    ###from .fallback import generate_rsa_keypair, RSA_KEY_CLASS
-    ##from .fallback import get_random_bytes, pad, unpad
-
-    # DUMMY PLACEHOLDERS SO THAT ERRORS DON'T POP IMEMDIATELY WHEN LOADING WACRYPTOLIB #
-
-    '''
-    class NotImplementedClass:
-        pass
-
-    def not_implemented_function(*args, **kwargs):
-        raise NotImplementedError("No backend utility found for this functionality")
-
-    import_rsa_key_from_pem = export_rsa_key_to_pem = not_implemented_function  #FIXMEEE
-
-    encrypt_via_aes_cbc = decrypt_via_aes_cbc = encrypt_via_aes_eax = decrypt_via_aes_eax = encrypt_via_chacha20_poly1305 = decrypt_via_chacha20_poly1305 = build_rsa_oaep_cipher = build_aes_cbc_cipher = build_aes_eax_cipher = build_chacha20_poly1305_cipher = not_implemented_function
-
-    generate_dsa_keypair = generate_ecc_keypair = import_dsa_key_from_pem = import_ecc_key_from_pem = export_dsa_key_to_pem = export_ecc_key_to_pem = not_implemented_function
-
-    DSA_KEY_CLASS = ECC_KEY_CLASS = NotImplementedClass
-
-    get_hasher_instance = not_implemented_function
-    shamir_split = shamir_combine = not_implemented_function
-    sign_with_pss = verify_with_pss = sign_with_dss = verify_with_dss = not_implemented_function
-    '''
